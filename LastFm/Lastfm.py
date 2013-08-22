d50c2f7bccfcc85b0a31feaf3d2bcb311b3172e2
rxsegrxup <mrmchase08@gmail.com>
2013-08-22 18:01:33 -0400

    identifier update

--- LastFm/Lastfm.py
+++ LastFm/Lastfm.py
@@ -28,7 +28,7 @@ lastfm.user_agent = '%s +http://xdm.lad1337.de' % common.getVersionHuman()
 
 class Lastfm(Provider):
     version = "0.19"
-    identifier = "de.rxse.lastfm"
+    identifier = "rxse.lastfm"
     _tag = 'lastfm'
     single = True
     types = ['de.lad1337.music']
